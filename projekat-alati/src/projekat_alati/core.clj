66dc8d721b4b22fc06e44513b7fdbc55d601e2e5
Moryf <aleksa.kb24@gmail.com>
2024-11-21 17:44:05 +0100

    Kreirane funkcije za sigmoid i tanh funkcije prelaza kai i njihovi izvodi i funkcija za nasumicno biranje tezine za Neurone

--- projekat-alati/src/projekat_alati/core.clj
+++ projekat-alati/src/projekat_alati/core.clj
@@ -1,7 +1,29 @@
 (ns projekat-alati.core
   (:gen-class))
+  (require '[clojure.math :as math])
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn sigmoid [x]
+(/ 1(+ 1 (math/pow Math/E (- x)))))
+
+(defn sigmoid-derivitive [x]
+(* (sigmoid x) (- 1 (sigmoid x))))
+
+(defn tanh [x]
+(/ (- (math/pow Math/E (* 2 x)) 1) (+ (math/pow Math/E (* 2 x)) 1)))
+
+(defn tanh-derivative [x]
+  (- 1 (math/pow (tanh x) 2)))
+
+(defn random-weight []
+  (- (* 2 (rand)) 1))
+
+(sigmoid 1)
+
+(sigmoid-derivitive 0.7310585786300049)
+
+(tanh 1)
+
+(tanh-derivative 0.761594155955765)
+
+
